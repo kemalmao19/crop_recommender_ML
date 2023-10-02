8d6e409aeac4429554b5c942b5f7e732c870f6d8
kemalmao19 <sebelumtidur14@gmail.com>
2023-10-03 06:16:49 +0700

    use lambda

--- deploy/recommender.py
+++ deploy/recommender.py
@@ -1,23 +1,11 @@
-# Libraries
 import warnings
-
-
-## Data processing
-import pandas as pd, numpy as np
-
-## Preprocessing
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-
-## Model
 from sklearn.linear_model import LogisticRegression
 
-
 # Preprocessing
-def readFile(path):
-    data = pd.read_csv(path)
-    return data
-
+readFile = lambda path: pd.read_csv(path)
 
 def xySplitter(data):
     cols = data.columns
@@ -26,62 +14,43 @@ def xySplitter(data):
     return X, y
 
 # Training
-def trainTest(x, y):
-    X_train, X_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.3, random_state=42
-    )
-    return X_train, X_test, y_train, y_test
+trainTest = lambda x, y: train_test_split(x, y, test_size=0.3, random_state=42)
 
+modeling = lambda model, X_train, y_train: model.fit(X_train, y_train)
 
-def modeling(model, X_train, y_train):
-    return model.fit(X_train, y_train)
+crop_names = {
+    13: "rice",
+    9: "maize",
+    0: "soybeans",
+    3: "beans",
+    12: "peas",
+    8: "groundnuts",
+    6: "cowpeas",
+    2: "banana",
+    10: "mango",
+    7: "grapes",
+    14: "watermelon",
+    1: "apple",
+    11: "orange",
+    5: "cotton",
+    4: "coffee",
+}
 
 # Predicting
-def predictData(model, data):
-    prediction = model.predict(data)
-    # accuracy = accuracy_score(y_test, prediction)
-    
-    print("=" * 60)
-    if prediction == 13:
-        print('Crop Recomendation: rice')
-    elif prediction == 9:
-        print('Crop Recomendation: maize')
-    elif prediction == 0:
-        print('Crop Recomendation: soybeans')
-    elif prediction == 3:
-        print('Crop Recomendation: beans')
-    elif prediction == 12:
-        print('Crop Recomendation: peas')
-    elif prediction == 8:
-        print('Crop Recomendation: groundnuts')
-    elif prediction == 6:
-        print('Crop Recomendation: cowpeas')
-    elif prediction == 2:
-        print('Crop Recomendation: banana')
-    elif prediction == 10:
-        print('Crop Recomendation: mango')
-    elif prediction == 7:
-        print('Crop Recomendation: grapes')
-    elif prediction == 14:
-        print('Crop Recomendation: watermelon')
-    elif prediction == 1:
-        print('Crop Recomendation: apple')
-    elif prediction == 11:
-        print('Crop Recomendation: orange')
-    elif prediction == 5:
-        print('Crop Recomendation: cotton')
-    elif prediction == 4:
-        print('Crop Recomendation: coffee')
-    # print("Accuracy Score:", accuracy)
-    print("=" * 60)
-
+predictData = lambda model, data: print(
+    "=" * 60,
+    f'Crop Recommendation: {crop_names.get(model.predict(data)[0], "Unknown")}',
+    "=" * 60,
+)
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
-    
-    path_data = "/home/kemal/Documents/project/dataset/Crop_recommendation.csv"
+
+    path_data = "/Users/kemalmao/crop_recommender_ML/dataset/Crop_recommendation.csv"
     path_current = input("input the data path: ")
-    current_data = pd.read_csv(path_current) #"/home/kemal/Documents/project/deploy/test.csv"
+    current_data = pd.read_csv(
+        path_current
+    )  # "/home/kemal/Documents/project/deploy/test.csv"
 
     data = readFile(path_data)
     X, y = xySplitter(data)
@@ -91,7 +60,7 @@ if __name__ == "__main__":
 
     model = LogisticRegression()
     trained_model = modeling(model, X_train, y_train)
-    
+
     print("=" * 60)
     print(current_data)
     predictData(trained_model, current_data)
